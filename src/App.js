961b62cc441bbfc5bb61d1f6ab32c791fd612876
anas <anasyoussif3@gmail.com>
2024-05-06 22:50:49 +0300

    car commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react'
 import './App.css';
+import Background from './components/Background/Background';
+import Navbar from './components/Navbar/Navbar';
+import Hero from './components/Hero/Hero';
 
-function App() {
+const App = () => {
+  let heroData = [
+    {text1:"Dive into",text2:"what you love"},
+    {text1:"Indulge",text2:"your passions"},
+    {text1:"Give in to",text2:"your passions"},
+  ]
+
+  const [heroCount,setHeroCount] = useState(0)
+  const [playStatus,setPlayStatus] = useState(false)
+
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar/>
+      <Hero
+      setPlayStatus={setPlayStatus}
+      heroData={heroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
